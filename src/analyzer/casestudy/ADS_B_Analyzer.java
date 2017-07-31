728094e99bcee475721d60861fb3814881a7b646
asci02 <asci02@I-PC03.asc-sys.local>
2017-07-31 13:57:34 +0900

    ADS_B_Analyzerクラスにprivate変数作成
    可読性向上のため、数値を定数化

--- src/analyzer/casestudy/ADS_B_Analyzer.java
+++ src/analyzer/casestudy/ADS_B_Analyzer.java
@@ -5,13 +5,22 @@ package analyzer.casestudy;
  */
 public class ADS_B_Analyzer {
 
+	private static final int TYPE_CODE_START_POSTION 	= 88;
+	private static final int TYPE_CODE_END_POSTION 	= 93;
+	private static final int MODES_ADDR_START_POSTION 	= 64;
+	private static final int MODES_ADDR_END_POSTION 	= 88;
+	private static final int CRC_START_POSTION			= 144;
+	private static final int CRC_END_POSTION 			= 168;
+
+
+
 	/**
 	 * dataからTypeCcodeを抽出
 	 * @param data SBS-3受信データ(バイナリ形式)
 	 * @return タイプコード番号
 	 */
 	public static int tc_Analyze(String data) {
-		return Integer.parseInt(data.substring(88,88+5), 2);
+		return Integer.parseInt(data.substring(TYPE_CODE_START_POSTION,TYPE_CODE_END_POSTION), 2);
 	}
 
 	/**
@@ -20,7 +29,7 @@ public class ADS_B_Analyzer {
 	 * @return モードSアドレス
 	 */
 	public static String modeS_Analyze(String data) {
-		 return Integer.toHexString(Integer.parseInt(data.substring(64,64+24), 2));
+		 return Integer.toHexString(Integer.parseInt(data.substring(MODES_ADDR_START_POSTION,MODES_ADDR_END_POSTION), 2));
 	}
 
 	/**
@@ -29,7 +38,8 @@ public class ADS_B_Analyzer {
 	 * @return パリティチェックの結果
 	 */
 	public static boolean parityCheck(String data){
-		return Integer.parseInt(data.substring(144, 168),2) == 0;
+		return Integer.parseInt(data.substring(CRC_START_POSTION, CRC_END_POSTION),2) == 0;
 	}
+
 
 }
