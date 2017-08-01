694b5d4749c5539c4c74cc0daffbc25b25ebad8e
asci02 <asci02@I-PC03.asc-sys.local>
2017-08-01 15:51:47 +0900

    DB関係のテスト等を追加

--- tst/casestudy/ConnectionManagerTest.java
+++ tst/casestudy/ConnectionManagerTest.java
@@ -1,9 +1,13 @@
 package casestudy;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
 
 import org.junit.Test;
 
@@ -22,6 +26,34 @@ public class ConnectionManagerTest {
 				fail();
 			}
 	}
+	@Test
+	public void CallSignTableに接続して数を数えるtest() {
+		int actual = 0;
+		Connection con = null;
+		try {
+			con = ConnectionManager.getConnection();
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery("SELECT modes FROM CALLSIGN");
+			LinkedList<String> result = new LinkedList<>();
+			while(rs.next()){
+				result.add(rs.getString(1));
+			}
+			actual = result.size();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		}
+		assertThat(actual, is(3));
+
+	}
+
 
 
 }
