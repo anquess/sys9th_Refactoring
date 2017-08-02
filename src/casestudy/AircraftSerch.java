e4bb6ee0a9bf6040592913fafd6676b48f3bef03
asci02 <asci02@I-PC03.asc-sys.local>
2017-08-02 10:40:12 +0900

    AircraftSerchにveloSetAttributeとveloToStringメソッドを追加
    runメソッドの可読性向上のため

--- src/casestudy/AircraftSerch.java
+++ src/casestudy/AircraftSerch.java
@@ -58,113 +58,96 @@ public class AircraftSerch  extends Thread {
 
 				if(posi == null || call == null || velo == null ){
 					System.out.println("そのような航空機はいません");
-
 				}else{
-
 					int i=0;
 					int k=0;
+						if(posi[0]==null){
+							System.out.println("Posi is null");
+						}
+						if(velo[0]==null){
+							System.out.println("Vello is null");
+						}
+						if(call[0]==null){
+							System.out.println("Call is null");
+						}
 
-						if(posi[0]==null){System.out.println("Posi is null");}
-						if(velo[0]==null){System.out.println("Vello is null");}
-						if(call[0]==null){System.out.println("Call is null");}
 						while(posi[i]!=null){
-							if(call[i]==null || posi[i]==null || velo[i]==null){System.out.println("Null\n\n");}
-							else{
-							System.out.println(posi[i].getModes());
-							System.out.println(call[i].getCallsign());
-							System.out.println(posi[i].getLat());
-							System.out.println(posi[i].getLng());
-							System.out.println(posi[i].getAlt());
-							System.out.println(velo[i].getH_velo());
-							System.out.println(velo[i].getV_velo());
-							System.out.println(velo[i].getH_dir());
-							System.out.println(velo[i].getV_dir());
-							System.out.println();
+							if(call[i]==null || posi[i]==null || velo[i]==null){
+								System.out.println("Null\n\n");
+							}else{
+								System.out.println(posi[i].getModes());
+								System.out.println(call[i].getCallsign());
+								System.out.println(posi[i].getLat());
+								System.out.println(posi[i].getLng());
+								System.out.println(posi[i].getAlt());
+								System.out.println(velo[i].getH_velo());
+								System.out.println(velo[i].getV_velo());
+								System.out.println(velo[i].getH_dir());
+								System.out.println(velo[i].getV_dir());
+								System.out.println();
 							}
 							i++;
-						  }
+						}
 
-						 // Documentインスタンスの生成
-				          DocumentBuilder documentBuilder = null;
-				          try {
-				               documentBuilder = DocumentBuilderFactory.newInstance()
-				                         .newDocumentBuilder();
+						// Documentインスタンスの生成
+						DocumentBuilder documentBuilder = null;
+						try {
+							documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+						} catch (ParserConfigurationException e) {
+							e.printStackTrace();
+						}
 
-				          } catch (ParserConfigurationException e) {
-				               e.printStackTrace();
-				          }
-				          Document document = documentBuilder.newDocument();
+						Document document = documentBuilder.newDocument();
+						i=0;
+						Element aircrafts = document.createElement("aircrafts");
+						document.appendChild(aircrafts);
 
-				          i=0;
+						while(posi[i]!=null){
+							System.out.println("XML1機分作成");
+							Element aircraft = document.createElement("aircraft");
+							aircrafts.appendChild(aircraft);
+							aircraft.setAttribute("modeSaddress",posi[i].getModes());
 
-				          Element aircrafts = document.createElement("aircrafts");
-				          document.appendChild(aircrafts);
+							if(call[i]!=null){
+								aircraft.setAttribute("callsign",call[i].getCallsign() );
+							}else{
+								aircraft.setAttribute("callsign","????????");
+							}
 
-				          while(posi[i]!=null){
+							aircraft.setAttribute("latitude",String.valueOf(posi[i].getLat()));
+							aircraft.setAttribute("longitude",String.valueOf(posi[i].getLng()));
+							aircraft.setAttribute("altitude",String.valueOf(posi[i].getAlt()));
 
-						  		System.out.println("XML1機分作成");
+							veloSetAttribute(velo[i], aircraft);
 
-								Element aircraft = document.createElement("aircraft");
-								aircrafts.appendChild(aircraft);
-						          aircraft.setAttribute("modeSaddress",posi[i].getModes());
-						          if(call[i]!=null){
-						          aircraft.setAttribute("callsign",call[i].getCallsign() );
-						          }else{aircraft.setAttribute("callsign","????????");}
-						          aircraft.setAttribute("latitude",String.valueOf(posi[i].getLat()));
-						          aircraft.setAttribute("longitude",String.valueOf(posi[i].getLng()));
-						          aircraft.setAttribute("altitude",String.valueOf(posi[i].getAlt()));
-						          if(velo[i]!=null){
-						          aircraft.setAttribute("h_velo",String.valueOf(velo[i].getH_velo()));
-						          aircraft.setAttribute("v_velo",String.valueOf(velo[i].getV_velo()));
-						          aircraft.setAttribute("h_dir",String.valueOf(velo[i].getH_dir()));
-						          aircraft.setAttribute("v_dir",String.valueOf(velo[i].getV_dir()));
-						          }else{
-						          aircraft.setAttribute("h_velo","????????");
-						          aircraft.setAttribute("v_velo","????????");
-						          aircraft.setAttribute("h_dir","????????");
-						          aircraft.setAttribute("v_dir","????????");}
+							aircraft.setAttribute("timestamp",String.valueOf(posi[i].getTimestamp()));
+							i++;
+						}
 
-						          aircraft.setAttribute("timestamp",String.valueOf(posi[i].getTimestamp()));
+						// XMLファイルの作成
+						File file = new File(Beans.getPwd()+"/N1/radar/Aircraft.xml");
+						file.delete();
+						file.createNewFile();
+						write(file, document);
 
-						          i++;
-				          }
+						for(k=0;k<i;k++){
+							posi[k]=null;
+							velo[k]=null;
+							call[k]=null;
+						}
 
-
-
-				          // XMLファイルの作成
-
-				          File file = new File(Beans.getPwd()+"/N1/radar/Aircraft.xml");
-				          file.delete();
-				          file.createNewFile();
-				          write(file, document);
-
-				          for(k=0;k<i;k++){
-				        	  posi[k]=null;
-				        	  velo[k]=null;
-				        	  call[k]=null;
-				          }
-
-
-				          i=0;
-				          k=0;
-
-
-
+						i=0;
+						k=0;
 				}
-
 			}catch(SQLException | InterruptedException e){
 							e.printStackTrace();
-
-			} catch (IOException e) {
-				// TODO 自動生成された catch ブロック
+			}catch (IOException e) {
 				e.printStackTrace();
-
-			} catch (Throwable e) {
-				// TODO 自動生成された catch ブロック
+			}catch (Throwable e) {
 				e.printStackTrace();
 			}
 
-		}
+		}	// While文終了
 
 		try{
 			if(con != null){
@@ -179,6 +162,29 @@ public class AircraftSerch  extends Thread {
 
 
 	}
+
+	private void veloSetAttribute(Velocity2 velo, Element aircraft) {
+		String subjects[] = {"h_velo","v_velo","h_dir","v_dir"};
+		for(String subject:subjects){
+			aircraft.setAttribute("h_velo",veloToString(velo,subject));
+		}
+	}
+	private String veloToString(Velocity2 velo, String subject){
+		if(velo == null){
+			return "????????";
+		}else if(subject == "h_velo"){
+			return String.valueOf(velo.getH_velo());
+		}else if(subject == "v_velo"){
+			return String.valueOf(velo.getV_velo());
+		}else if(subject == "h_dir"){
+			return String.valueOf(velo.getH_dir());
+		}else if(subject == "v_dir"){
+			return String.valueOf(velo.getV_dir());
+		}else{
+			throw new IllegalArgumentException();
+		}
+	}
+
 
 	private boolean write(File file, Document document) {
 
@@ -211,4 +217,3 @@ public class AircraftSerch  extends Thread {
 
 
 }
-
