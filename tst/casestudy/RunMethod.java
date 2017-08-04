79c97d145fdc393c59b66ba784420d99004d2855
asci02 <asci02@I-PC03.asc-sys.local>
2017-08-04 13:54:47 +0900

    DB接続をスレッドによるテストを作成(ただし、JUnit未使用)

---
+++ tst/casestudy/RunMethod.java
@@ -0,0 +1,49 @@
+package casestudy;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * AirCraftSerchのrunメソッドにおいて、
+ * ConnectionManagerのげｔConnectionでエラーとなるため、
+ * スレッドが原因か特定するためのモックメソッド
+ */
+public class RunMethod extends Thread {
+
+	/**
+	 * スレッド内の無限ループのon,off
+	 * trueなら無限ループ、falseなら無限ループからでる。
+	 */
+	private boolean loop = true;
+
+	/**
+	 * AirCraftSerchのrunメソッドにおいて、
+	 * ConnectionManagerのげｔConnectionでエラーとなるため、
+	 * スレッドが原因か特定するためのモックメソッド
+	 */
+	@Override
+	public void run(){
+		Connection con = null;
+		while(loop){
+			try {
+				con = ConnectionManager.getConnection();
+			}catch(Exception e){
+				this.loop = false;
+				e.printStackTrace();
+			}
+			System.out.println("接続完了");
+		}
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("接続終了");
+	}
+
+	public void setLoop(boolean loop){
+		this.loop = loop;
+	}
+
+
+}
