13fa97b571007efe3eaf1fee0c90819945c35717
asct06 <asct06@T-PC06.asc-sys.local>
2017-08-18 12:58:32 +0900

    GetDaoクラスに抽象メソッドgetSqlを作成
    SQLを直接参照ではなく、getメソッド経由とし、再利用性を高めるため

--- src/n4/dao/GetDao.java
+++ src/n4/dao/GetDao.java
@@ -1,5 +1,14 @@
 package n4.dao;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import casestudy.DbItem;
+
 public abstract class GetDao extends N1PramDao {
+
+	public abstract void executeQuery(DbItem[] dbItem, ResultSet result,int i) throws SQLException;
+
+	public abstract String getSql() ;
 
 }
--- src/n4/dao/GetPositionDao.java
+++ src/n4/dao/GetPositionDao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import casestudy.DbItem;
 import casestudy.Position;
 
-public class GetPositionDao extends N1PramDao {
+public class GetPositionDao extends GetDao {
 
 	private  final String SQL = "select * from position where timestamp in (select max(timestamp) from position group by modes)"
 				+ "and  timestamp > systimestamp-0.0008";
@@ -16,33 +16,31 @@ public class GetPositionDao extends N1PramDao {
 		Position position[] = new Position[100];
 		try{
 			open();
-			super.setSql(SQL);
+			super.setSql(getSql());
 			PreparedStatement stmt = super.getStmt();
 			ResultSet result = stmt.executeQuery();
-
-			executeQuery(position, result);
+			int i=0;
+			while(result.next()){
+				executeQuery(position, result,i);
+				i++;
+			}
 
 		}finally{
 			close();
-
 		}
 		return position;
 	}
 
-	public void executeQuery(DbItem[] position, ResultSet result) throws SQLException {
-		int i=0;
-		while(result.next()){
-
-			position[i] = new Position(
-					result.getString("modes"),
-					result.getFloat("latitude"),
-					result.getFloat("longitude"),
-					result.getFloat("altitude"),
-					result.getTimestamp("timestamp")
-					);
-			i++;
-
-		}
+	@Override
+	public void executeQuery(DbItem[] position, ResultSet result,int i) throws SQLException {
+		position[i] = new Position(	result.getString("modes"),result.getFloat("latitude"),
+				result.getFloat("longitude"),result.getFloat("altitude"),result.getTimestamp("timestamp"));
 	}
+
+	@Override
+	public String getSql(){
+		return SQL;
+	}
+
 
 }
