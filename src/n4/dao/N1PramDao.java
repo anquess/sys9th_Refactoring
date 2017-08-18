b014024fcb5cca91cb28367f2e5ea934c7435829
asct06 <asct06@T-PC06.asc-sys.local>
2017-08-18 11:45:50 +0900

    N4Daoを継承したN1PramDaoの作成
    DBの接続パラメータを渡す

---
+++ src/n4/dao/N1PramDao.java
@@ -0,0 +1,16 @@
+package n4.dao;
+
+public abstract class N1PramDao extends N4Dao {
+
+	private static final String server = "192.168.1.17";
+	private static final int portNum = 1521;
+	private static final String dbName = "ascpdb01";
+	private static final String user= "ASC45th";
+	private static final String password ="system";
+
+	public N1PramDao() {
+		super(server, portNum, dbName, user, password);
+
+	}
+
+}
