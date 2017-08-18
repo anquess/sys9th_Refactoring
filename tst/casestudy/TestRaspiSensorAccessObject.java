0fababa30925790121764ae8cadb9f35ed348d9a
asct06 <asct06@T-PC06.asc-sys.local>
2017-08-18 16:18:38 +0900

    raspiへの接続確認用
    SBS3をコピー

---
+++ tst/casestudy/TestRaspiSensorAccessObject.java
@@ -0,0 +1,28 @@
+package casestudy;
+
+public class TestRaspiSensorAccessObject {
+
+	private static final String ipAddress = "192.168.3.172";
+	private static final int portNumber = 30002;
+
+	/**
+	 * @param args 不使用
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		SensorAccessObject testSOA = null;
+		testSOA = new SensorAccessObject(ipAddress, portNumber);
+
+		testSOA.connect();
+		for(int i=0;i < 100; i++){
+			String hex = testSOA.readSensor();
+			System.out.println(hex);
+		}
+
+		testSOA.close();
+
+
+	}
+
+}
