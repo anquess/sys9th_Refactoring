3e1a25695d4d029a7e35ca8c0e8b532b1317dce1
asct06 <asct06@T-PC06.asc-sys.local>
2017-09-05 08:20:10 +0900

    ブランチ切ったのでTGパッケージ削除
    developへ移行

--- src/n4/TG/AircraftAdvisory/AdvisoryObj.java
+++
@@ -1,7 +0,0 @@
-package n4.TG.AircraftAdvisory;
-
-import n4.TG.framework.AircraftObj;
-
-public class AdvisoryObj extends AircraftObj  {
-
-}
--- src/n4/TG/framework/DocumentBuider.java
+++
@@ -1,5 +0,0 @@
-package n4.TG.framework;
-
-public interface DocumentBuider {
-
-}
